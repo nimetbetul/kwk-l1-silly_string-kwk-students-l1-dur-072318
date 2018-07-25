d01b60e490084b1e7c2ddf1dfb9416a2b992fd77
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-07-25 04:34:23 +0000

    automatically backed up by learn

--- random.rb
+++ random.rb
@@ -1,2 +1,4 @@
-sisters=["Nimet", "Banu", "Sevde"]
-puts sisters.insert("best")
+def say_hello (your_name, my_name)
+  puts "Hello #{your_name}! I'm #{my_name}. It is very nice to meet you."
+end
+say_hello ("Yasemin", "Nimet")
