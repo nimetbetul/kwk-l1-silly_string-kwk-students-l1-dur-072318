2c872b0481a11f3f678b3cf1502a8d33c24bd0bb
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-07-25 04:37:59 +0000

    automatically backed up by learn

--- random.rb
+++ random.rb
@@ -1,4 +1,4 @@
 def say_hello (your_name, my_name)
   puts "Hello #{your_name}! I'm #{my_name}. It is very nice to meet you. How old are you?"
 end
-say_hello("Murtaza","Ali")
+say_hello("Nimet","Samantha")
