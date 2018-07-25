f8388b32eece8f0d6857a8140572d3ca3aea50c8
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-07-25 04:36:06 +0000

    automatically backed up by learn

--- random.rb
+++ random.rb
@@ -1,4 +1,4 @@
 def say_hello (your_name, my_name)
-  puts "Hello #{your_name}! I'm #{my_name}. It is very nice to meet you."
+  puts "Hello #{your_name}! I'm #{my_name}. It is very nice to meet you. How old are you?"
 end
 say_hello("Inci","Osman")
